87413ce90ee673a3860e6f774dbd5b2628b2bd9c
zhujinxuan <zhujinxuan@gmail.com>
2023-11-02 15:16:59 -0400

    Add shell alias

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
         # List packages installed in system profile. To search by name, run:
         # $ nix-env -qaP | grep wget
         environment.systemPackages = [ pkgs.vim ];
+        environment.shellAliases = { ll = "ls -l"; };
 
         # Auto upgrade nix package and the daemon service.
         services.nix-daemon.enable = true;
