e0de3bcbb68ace49254ad0db2990dd9c500772cb
zhujinxuan <zhujinxuan@gmail.com>
2023-11-02 15:11:51 -0400

    Sync

--- flake.nix
+++ flake.nix
@@ -39,9 +39,41 @@
         # Create /etc/zshrc that loads the nix-darwin environment.
         programs.zsh.enable = true; # default shell on catalina
 
-        programs.skhd = {
+        services.skhd = {
           enable = true;
           skhdConfig = builtins.readFile ./skhdrc;
+        };
+
+        services.yabai = {
+          enable = true;
+          config = {
+            mouse_follows_focus = "on";
+            focus_follows_mouse = "off";
+            window_placement = "second_child";
+            window_topmost = "off";
+            window_shadow = "on";
+            window_opacity = "off";
+            window_opacity_duration = 0.0;
+            active_window_opacity = 1.0;
+            normal_window_opacity = 0.9;
+            window_border = "off";
+            window_border_width = 6;
+            active_window_border_color = "0xff775759";
+            normal_window_border_color = "0xff555555";
+            insert_feedback_color = "0xffd75f5f";
+            split_ratio = 0.5;
+            auto_balance = "off";
+            mouse_modifier = "fn";
+            mouse_action1 = "move";
+            mouse_action2 = "resize";
+            mouse_drop_action = "swap";
+            layout = "bsp";
+            top_padding = 0;
+            bottom_padding = 0;
+            left_padding = 0;
+            right_padding = 0;
+            window_gap = 6;
+          };
         };
 
         # Set Git commit hash for darwin-version.
