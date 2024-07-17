3bd12db2a8b81137a2810a5a5fd889343611f54f
zhujinxuan <zhujinxuan@gmail.com>
2024-07-17 17:29:03 -0400

    Add podman

---
+++ common.nix
@@ -0,0 +1,89 @@
+{ pkgs, ... }:
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = [ pkgs.vim ];
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  nix.package = pkgs.nix;
+
+  nix.settings = {
+    # Necessary for using flakes on this system.
+    experimental-features = "nix-command flakes";
+    trusted-substituters = [
+      "https://cache.nixos.org"
+      "https://haskell-language-server.cachix.org"
+      "https://emacs-osx.cachix.org"
+      "https://nequissimus.cachix.org"
+      "https://devenv.cachix.org"
+    ];
+  };
+
+  # do garbage collection weekly to keep disk usage low
+  nix.gc = {
+    automatic = true;
+    interval = {
+      Day = 7;
+    };
+    options = "--delete-older-than 1w";
+  };
+
+  # You can enable the following option to migrate to new style nixbld users
+  nix.configureBuildUsers = true;
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfreePredicate = _: true;
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true; # default shell on catalina
+
+  services.skhd = {
+    enable = true;
+    skhdConfig = builtins.readFile ./skhdrc;
+  };
+
+  services.yabai = {
+    enable = true;
+    config = {
+      mouse_follows_focus = "on";
+      focus_follows_mouse = "off";
+      window_placement = "second_child";
+      window_topmost = "off";
+      window_shadow = "on";
+      window_opacity = "off";
+      window_opacity_duration = 0.0;
+      active_window_opacity = 1.0;
+      normal_window_opacity = 0.9;
+      window_border = "off";
+      window_border_width = 6;
+      active_window_border_color = "0xff775759";
+      normal_window_border_color = "0xff555555";
+      insert_feedback_color = "0xffd75f5f";
+      split_ratio = 0.5;
+      auto_balance = "off";
+      mouse_modifier = "fn";
+      mouse_action1 = "move";
+      mouse_action2 = "resize";
+      mouse_drop_action = "swap";
+      layout = "bsp";
+      top_padding = 0;
+      bottom_padding = 0;
+      left_padding = 0;
+      right_padding = 0;
+      window_gap = 6;
+    };
+  };
+
+  # fonts
+  fonts.packages = with pkgs; [
+    rubik
+    nerdfonts
+    fira-code
+    fira-code-symbols
+    emacs-all-the-icons-fonts
+  ];
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
--- flake.nix
+++ flake.nix
@@ -14,107 +14,17 @@
       nixpkgs,
     }:
     let
-      configuration =
-        { pkgs, ... }:
-        {
-          # List packages installed in system profile. To search by name, run:
-          # $ nix-env -qaP | grep wget
-          environment.systemPackages = [ pkgs.vim ];
-          environment.shellAliases = {
-            ll = "ls -l";
-          };
+      platformM1 = _: {
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
+      platformIntel = _: {
 
-          # Auto upgrade nix package and the daemon service.
-          services.nix-daemon.enable = true;
-          nix.package = pkgs.nix;
-
-          nix.settings = {
-            # Necessary for using flakes on this system.
-            experimental-features = "nix-command flakes";
-            trusted-substituters = [
-              "https://cache.nixos.org"
-              "https://hercules-ci.cachix.org"
-              "https://haskell-language-server.cachix.org"
-              "https://emacs-osx.cachix.org"
-              "https://srid.cachix.org"
-              "https://nequissimus.cachix.org"
-              "https://devenv.cachix.org"
-            ];
-          };
-
-          # do garbage collection weekly to keep disk usage low
-          nix.gc = {
-            automatic = true;
-            interval = {
-              Day = 7;
-            };
-            options = "--delete-older-than 1w";
-          };
-
-          # You can enable the following option to migrate to new style nixbld users
-          nix.configureBuildUsers = true;
-
-          nixpkgs.config.allowUnfree = true;
-          nixpkgs.config.allowUnfreePredicate = _: true;
-
-          # Create /etc/zshrc that loads the nix-darwin environment.
-          programs.zsh.enable = true; # default shell on catalina
-
-          services.skhd = {
-            enable = true;
-            skhdConfig = builtins.readFile ./skhdrc;
-          };
-
-          services.yabai = {
-            enable = true;
-            config = {
-              mouse_follows_focus = "on";
-              focus_follows_mouse = "off";
-              window_placement = "second_child";
-              window_topmost = "off";
-              window_shadow = "on";
-              window_opacity = "off";
-              window_opacity_duration = 0.0;
-              active_window_opacity = 1.0;
-              normal_window_opacity = 0.9;
-              window_border = "off";
-              window_border_width = 6;
-              active_window_border_color = "0xff775759";
-              normal_window_border_color = "0xff555555";
-              insert_feedback_color = "0xffd75f5f";
-              split_ratio = 0.5;
-              auto_balance = "off";
-              mouse_modifier = "fn";
-              mouse_action1 = "move";
-              mouse_action2 = "resize";
-              mouse_drop_action = "swap";
-              layout = "bsp";
-              top_padding = 0;
-              bottom_padding = 0;
-              left_padding = 0;
-              right_padding = 0;
-              window_gap = 6;
-            };
-          };
-
-          # Set Git commit hash for darwin-version.
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-
-          # fonts
-          fonts.packages = with pkgs; [
-            rubik
-            nerdfonts
-            fira-code
-            fira-code-symbols
-            emacs-all-the-icons-fonts
-          ];
-          # Used for backwards compatibility, please read the changelog before changing.
-          # $ darwin-rebuild changelog
-          system.stateVersion = 4;
-        };
-
-      platformM1 = _: { nixpkgs.hostPlatform = "aarch64-darwin"; };
-      platformIntel = _: { nixpkgs.hostPlatform = "aarch64-darwin"; };
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
 
     in
     {
@@ -122,15 +32,16 @@
       # $ darwin-rebuild build --flake .#Jinxuan-MacBook-Pro
       darwinConfigurations."Jinxuan-MacBook-Pro" = nix-darwin.lib.darwinSystem {
         modules = [
-          configuration
+          ./common.nix
           platformIntel
         ];
       };
 
       darwinConfigurations."Jinxuan-MacBookM1-Pro" = nix-darwin.lib.darwinSystem {
         modules = [
-          configuration
+          ./common.nix
           platformM1
+          ./podman.nix
         ];
       };
 
